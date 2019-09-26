2367b7b0a301e97d49d17d04948dd633db22b97b
Jatin <jainjatin4146@gmail.com>
2019-09-26 10:12:53 +0530

    second commit

--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,4 @@
-    #Python program to multiply two numbers using function
-    def add_num(a,b):#function for multiplication
-        multiply=a*b;
-        return multiply; #return value
-    num1=int(input("input the number one: "))#input from user for num1
-    num2=int(input("input the number one: "))#input from user for num2
-    print("The product is",add_num(num1,num2))#call the function
+def mul(a,b):
+    multiply=a*b
+    return multiply
+
